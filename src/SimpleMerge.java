3d11fbfc6f1f50a55a1133530702f80ea1c411cc
BT_1N3_27 <kfang9357@bths.edu>
2019-01-31 08:29:39 -0500

    mergeHelper and mergeSort added

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -5,6 +5,43 @@
  * array of integers.
  */
 public class SimpleMerge {
+    private int[] arr1;
+    private int[] arr2;
+
+    public SimpleMerge (int[] arr1, int[] arr2)
+    {
+        this.arr1=arr1;
+        this.arr2 =arr2;
+    }
+
+    public static void simpleMerge()
+    {
+
+    }
+
+
+    private static void merge(int[] elements,int from, int mid, int to, int[] temp)
+        {
+            int i = from;
+            int j = mid + 1;
+            int k = from;
+            while (i <= mid && j <= to)
+            {
+                if (elements[i] < elements[j])
+                {
+                    temp[k] = elements[i];
+                    i++;
+                }
+                else
+                {
+                    temp[k] = elements[j];
+                    j++;
+                }
+                k++;
+            }
+    }
+
+
     public void mergeSort (int[] arr)
     {
         int n = arr.length;
@@ -12,10 +49,8 @@ public class SimpleMerge {
         mergeSortHelper (arr,0,n-1,temp);
     }
 
-    public void merge (int[]arr, int left, int mid, int right, int[] temp)
-{ }
 
- public void mergeSortHelper(int[]arr, int left, int right, int[]temp)
+    public void mergeSortHelper(int[]arr, int left, int right, int[]temp)
     {
         if (left<right)
         {
@@ -25,4 +60,5 @@ public class SimpleMerge {
             merge (arr, left, mid, right, temp);
         }
     }
+
 }
