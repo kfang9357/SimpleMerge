2b20e2a7370c3b373297041f29bebc6af7bbe7d2
BT_1N3_27 <kfang9357@bths.edu>
2019-01-31 08:14:48 -0500

    mergeHelper and mergeSort added

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -5,41 +5,24 @@
  * array of integers.
  */
 public class SimpleMerge {
-    public static int[] simpleMerge(int[] arr1, int[] arr2) {
-        int len1 = arr1.length;
-        int len2 = arr2.length;
-        int arr3[];
-        arr3 = new int[len1 + len2];
-        int j = 0;
-        while (j<len1+len2) {
-            for (int i = 0; i<len1; i++)
-            {
-                arr3[j] = arr1[i];
-                j++;
-            }
-            for (int k = 0; k<len1; k++)
-            {
-                arr3[j] = arr2[k];
-                j++;
-            }
-        }
-        for (int index = 0; index < arr3.length ; index ++)
-        {
-            for (int p = index; p > 0; j-- )
-            {
-                if (arr3[p]<arr3[p-1])
-                {
-                    swap(arr3, p,p-1);
-                }
-            }
-        }
-        return arr3;
+    public void mergeSort (int[] arr)
+    {
+        int n = arr.length;
+        int[] temp = new int [n];
+        mergeSortHelper (arr,0,n-1,temp);
     }
 
-    public static void swap (int[]arr, int i, int j)
+    public void merge (int[]arr, int left, int mid, int right, int[] temp)
+{ }
+
+ public void mergeSortHelper(int[]arr, int left, int right, int[]temp)
     {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        if (left<right)
+        {
+            int mid = (left + right) / 2;
+            mergeSortHelper(arr,left,mid,temp);
+            mergeSortHelper(arr, mid+1, right, temp);
+            merge (arr, left, mid, right, temp);
+        }
     }
 }
